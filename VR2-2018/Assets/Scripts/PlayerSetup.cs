f05196b5b2b5ff3830d6fd720cdf4271eb2cd3a1
Fred C <fchappuis452@gmail.com>
2020-03-03 14:51:40 -0500

    Tweak VR stuff changed menu panel location

--- VR2-2018/Assets/Scripts/PlayerSetup.cs
+++ VR2-2018/Assets/Scripts/PlayerSetup.cs
@@ -5,22 +5,23 @@ using Valve.VR.InteractionSystem;
 
 public class PlayerSetup : MonoBehaviour
 {
+    public GameObject menuPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (var hand in Player.instance.hands)
-        {
-            if (hand.name == "LeftHand")
-            {
-                hand.HideController();
-            }
+        //foreach (var hand in Player.instance.hands)
+        //{
+        //    if (hand.name == "LeftHand")
+        //    {
+        //        hand.HideController();
+        //    }
 
-        }
+        //}
     }
 }
