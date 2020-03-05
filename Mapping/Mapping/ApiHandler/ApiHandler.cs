ee993634b4eed3b321475ae9f4c3e33a82be4f33
Philip Kempton <pkempton3826@conestogac.on.ca>
2020-03-05 10:29:07 -0500

    re-attached Mapping solution to Fred's database, as apposed to Brendans laptop

--- Mapping/Mapping/ApiHandler/ApiHandler.cs
+++ Mapping/Mapping/ApiHandler/ApiHandler.cs
@@ -39,8 +39,8 @@ namespace Mapping.ApiHandler
             string jsonString = JsonSerializer.Serialize(obj);
 
             // create the stream to the API and make the call
-            //string apiCall = @"http://192.0.203.84:5000/db_api/addimg";
-            string apiCall = @"http://10.192.216.94:5000/db_api/addimg";
+            string apiCall = @"http://192.0.203.84:5000/db_api/addimg";
+            //string apiCall = @"http://10.192.216.94:5000/db_api/addimg";
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(apiCall);
             req.ContentType = "application/json; charset=utf-8";
             req.Method = "POST";
