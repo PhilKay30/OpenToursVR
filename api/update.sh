2210c269706014b3618ec5887a757a3561e1be45
Frederic Chappuis <fchappuis3050@conestogac.on.ca>
2020-03-09 23:20:59 -0400

    added a shell script to automate the process and system config files

---
+++ api/update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This will update the repo and restart nginx
+
+echo 'Pulling from repo'
+git pull
+
+echo 'Running Pipenv to get latest modules'
+pipenv install
+
+echo 'Restarting nginx and the daemon'
+sudo systemctl restart nginx
+sudo systemctl daemon-reload
+
