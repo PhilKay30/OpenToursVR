1ac6efa9ecdee0b8bf07d0e9bb49824ae46b7449
Timothy J Cowen <timothyjcowen@gmail.com>
2020-03-11 14:34:25 -0400

    Added empty page for historical map integration.

---
+++ Mapping/Mapping/HistoricalMap/HistoricalMapPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Mapping.HistoricalMap
+{
+    // ReSharper disable once UnusedMember.Global
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class HistoricalMapPage : Page
+    {
+        public HistoricalMapPage()
+        {
+            InitializeComponent();
+            this.Loaded += OnPageLoad;
+        }
+
+        /// <summary>
+        /// Handles updating the forward/back navigation buttons
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void OnPageLoad(object sender, RoutedEventArgs e)
+        {
+            (Application.Current.MainWindow as LaunchWindow)?.UpdateNavigation();
+        }
+    }
+}
--- Mapping/Mapping/LaunchPage.xaml.cs
+++ Mapping/Mapping/LaunchPage.xaml.cs
@@ -36,5 +36,11 @@ namespace Mapping
             NavigationService?.Navigate(new Uri(
                 "SvgConverter/SvgConverterPage.xaml", UriKind.Relative));
         }
+
+        private void OnClick_AddHistoricalMap(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new Uri(
+                "HistoricalMap/HistoricalMapPage.xaml", UriKind.Relative));
+        }
     }
 }
